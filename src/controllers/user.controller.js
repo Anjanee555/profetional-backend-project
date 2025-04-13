58e37fe62fdd03f4e6dfb20cf4d10b40dc1f2cdd
Anjanee555 <anjaneekumar76076@gmail.com>
2025-04-14 00:36:54 +0530

    Add getUserChannelProfile endpoint and improve response handling in existing endpoints

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -256,7 +256,7 @@ const changeCurrentPassword = asyncHandler(async (req,res) => {
 const getCurrentUser = asyncHandler(async (req,res) => {
     return res
     .status(200)
-    .json(200,req.user,"Current user fetched successfully")
+    .json(new ApiError(200,req.user,"Current user fetched successfully"))
 
 })
 
@@ -267,7 +267,7 @@ const updateAccountDetails = asyncHandler(async (req,res) => {
         throw new ApiError(400, "All fields are required")
     }
 
-    const user = User.findById(
+    const user = await User.findById(
         req.user?._id,
         {
             $set: {
@@ -337,7 +337,76 @@ const updateUserCoverImage = asyncHandler(async (req,res) => {
     .json(new ApiResponse(200, user, "User cover image updated successfully"))
 })
     
+const getUserChannelProfile  = asyncHandler(async (req,res) => {
+    const {username} = req.params
 
+    if(!username.trim()) {
+        throw new ApiError(400, "Username is missing")
+    }
+
+    const channel = await User.aggregate([
+        {
+            $match: {
+                username: username?.toLowerCase()
+            },
+        },
+        {
+            $lookup: {
+                from: "subscriptions",
+                localField: "_id",
+                foreignField: "channel",
+                as: "subscribers"
+                  
+            },
+        
+        },
+        {
+            $lookup: {
+                from: "subscriptions",
+                localField: "_id",
+                foreignField: "subscriber",
+                as: "subscribedTo"
+            }
+        },
+        {
+            $addFields: {
+                subscriberCount: { 
+                    $size: "$subscribers"
+                },
+                channelsSubscribedToCount: {
+                    $size: "$subscribedTo"
+                },
+                isSubscribed: {
+                    $cond: {
+                        if: { $in: [req.user?._id,"$subscribers.subscriber"] },
+                        then: true,
+                        else: false
+                    }
+                }
+            }
+        },
+        {
+            $project: {
+                fullName: 1,
+                username: 1,
+                subscriberCount: 1,
+                channelsSubscribedToCount: 1,
+                isSubscribed: 1,
+                coverImage: 1,
+                email: 1,
+            }
+        }
+    ])
+
+    if(!channel?.length) {
+        throw new ApiError(400, "Channel does not exist")
+    }
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200, channel[0], "User channel profile fetched successfully"))
+    
+})
 
 export 
 {
@@ -349,5 +418,6 @@ export
     getCurrentUser,
     updateAccountDetails,
     updateUserAvatar,
-    updateUserCoverImage
+    updateUserCoverImage,
+    getUserChannelProfile
 }
