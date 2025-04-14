057bf889b8d08cb52cac37fec6bb9225efa9921e
Anjanee555 <anjaneekumar76076@gmail.com>
2025-04-14 17:58:06 +0530

    Fix upload middleware usage for avatar route in user routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -45,7 +45,7 @@ router.route("/current-user").get(verifyJWT,getCurrentUser)
 
 router.route("/update-profile").patch(verifyJWT,updateAccountDetails)
 
-router.route("/avatar").patch(verifyJWT,upload("avatar"),updateUserAvatar)
+router.route("/avatar").patch(verifyJWT,upload.single("avatar"),updateUserAvatar)
 
 router.route("/cover-image").patch(verifyJWT,upload.single("/coverImage"),updateUserCoverImage)
 
